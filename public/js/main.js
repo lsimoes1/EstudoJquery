adbcdf080bffc47564111f874f1089ee8a0f2a92
Luan Simões <luan.simoes1@gmail.com>
2018-12-10 16:04:00 -0200

    Estudos aula 3-1 - Alura Eventos Jquery.

--- public/js/main.js
+++ public/js/main.js
@@ -11,3 +11,21 @@ $(".campo-digitacao").on("input", function(){
     $("#contador-palavras").text(qtdPalavras);
     $("#contador-caracteres").text(conteudo.length);
 });
+
+$(".campo-digitacao").one("focus", function(){
+
+    var tempoRestante = $("#tempo-digitacao").text();
+
+    var idInterval = setInterval(function(){
+        tempoRestante --;
+        console.log(tempoRestante);
+        $("#tempo-digitacao").text(tempoRestante);
+
+        if(tempoRestante <= 0)
+        {
+            $("#tempo-digitacao").attr('disabled', true);
+            clearInterval(idInterval);
+        }
+
+    },1000);
+});
